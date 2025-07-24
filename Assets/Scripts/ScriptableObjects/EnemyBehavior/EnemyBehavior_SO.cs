47581888b6a1f578567677facb97eca458e172a9
mierka_m <162184787+marsborox@users.noreply.github.com>
2025-07-24 23:11:09 +0200

    minor stuff towards SO architecture

---
+++ Assets/Scripts/ScriptableObjects/EnemyBehavior/EnemyBehavior_SO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyBehavior_SO", menuName = "Scriptable Objects/EnemyBehavior_SO")]
+public class EnemyBehavior_SO : ScriptableObject
+{
+    
+}
