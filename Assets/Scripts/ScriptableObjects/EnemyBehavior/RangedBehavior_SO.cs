1d396785b668b01d5174abf4cbdddbd70cd34dda
mierka_m <162184787+marsborox@users.noreply.github.com>
2025-08-06 23:04:38 +0200

    attempt to make Weapon_SO usable for enemies

--- Assets/Scripts/ScriptableObjects/EnemyBehavior/RangedBehavior_SO.cs
+++ Assets/Scripts/ScriptableObjects/EnemyBehavior/RangedBehavior_SO.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class RangedBehavior_SO : EnemyBehavior_SO
 {
     [SerializeField] Projectile _projectilePrefab;
+    [Tooltip("rangedAttackPattern")]
+    [SerializeField] Weapon_SO _rangedWeapon;
     public override void EnemyBehavior(Unit target, Unit usedEnemy)
     {
         var enemy = ((Enemy)usedEnemy);
@@ -11,7 +13,23 @@ public class RangedBehavior_SO : EnemyBehavior_SO
         {        
             if (IsInRange(target, usedEnemy))
             {
-                Attack(target, usedEnemy);
+                if (_rangedWeapon == null)
+                {
+                    Attack(target, usedEnemy);
+                }
+                else
+                {
+                    Transform attackDirection;
+                    
+                    ((Enemy)usedEnemy).attackReady = false;
+
+                    Vector2 targetPosition = (usedEnemy.transform.position -target.transform.position).normalized;
+                    Quaternion shootingDirection;/* = Quaternion.Euler(targetPosition.x,targetPosition.y,targetPosition.z);*/
+                    //Transform shootingTransform = usedEnemy.transform;
+                    shootingDirection = Quaternion.LookRotation(targetPosition);
+                    //shootingTransform.rotation = shootingDirection;
+                    _rangedWeapon.Attack(_projectilePrefab,usedEnemy, shootingDirection/*target.transform*/);
+                }
             }
             else
             {
@@ -42,5 +60,9 @@ public class RangedBehavior_SO : EnemyBehavior_SO
         spawnedProjectile.transform.up = -(usedEnemy.transform.position - shootPosition);
         spawnedProjectile.targetTag = ((Enemy)usedEnemy).targetTag;
         spawnedProjectile.damage = ((Enemy)usedEnemy).damage;
+    }
+    private void AttackFromSO()
+    { 
+        
     }
 }
