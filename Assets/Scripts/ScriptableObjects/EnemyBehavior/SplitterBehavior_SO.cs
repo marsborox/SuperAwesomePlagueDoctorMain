ce35ca54e1c234ed1da9a133427a3aa251d18a4c
mierka_m <162184787+marsborox@users.noreply.github.com>
2025-09-14 23:33:55 +0200

    splitter done

--- Assets/Scripts/ScriptableObjects/EnemyBehavior/SplitterBehavior_SO.cs
+++ Assets/Scripts/ScriptableObjects/EnemyBehavior/SplitterBehavior_SO.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "SplitterBehavior_SO", menuName = "Scriptable Objects/EnemyBehavior_SO/SplitterBehavior_SO")]
-public class SplitterBehavior_SO : EnemyBehavior_SO, I_MeleeAttack
+public class SplitterBehavior_SO : EnemyBehavior_SO, I_MeleeAttack,I_RandomPointOnDistance
 {
     [SerializeField] private Enemy _miniMePrefab;
     [SerializeField] private int _amountOfMiniMeSpawns;
     [SerializeField] private Enemy_SO _miniMe_SO;
+    [SerializeField] private float _miniMeMaxSpawnDistance;
     public override void EnemyBehavior(Unit target, Unit usedEnemy)
     {
         //Debug.Log("Enemy is Behaving Melee");
@@ -26,7 +27,11 @@ public class SplitterBehavior_SO : EnemyBehavior_SO, I_MeleeAttack
 
     public override void Die(Unit usedEnemy)
     {
-        usedEnemy.enemySpawner.SpawnEnemy(_miniMe_SO, _miniMePrefab,usedEnemy.transform);
+        for (int i = 0; i<_amountOfMiniMeSpawns; i++)
+        {
+            Transform transform = ((I_RandomPointOnDistance)this).ReturnRandomTransformOnDistance(usedEnemy.transform, _miniMeMaxSpawnDistance);
+            usedEnemy.enemySpawner.SpawnEnemy(_miniMe_SO, _miniMePrefab, usedEnemy.transform);
+        }
     }
 
 }
